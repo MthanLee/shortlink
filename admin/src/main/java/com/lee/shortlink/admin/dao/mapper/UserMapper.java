d2a5412262a546d4d4763f5744d6e4994505686e
Lee <2014140285@qq.com>
2023-11-17 19:42:57 +0800

    feature: 开发短链接后管系统相关功能

---
+++ admin/src/main/java/com/lee/shortlink/admin/dao/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.lee.shortlink.admin.dao.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lee.shortlink.admin.dao.entity.UserDO;
+
+/**
+ * 用户持久层
+ */
+public interface UserMapper extends BaseMapper<UserDO> {
+}
---
+++ admin/src/main/java/com/lee/shortlink/admin/service/impl/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package com.lee.shortlink.admin.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.lee.shortlink.admin.common.convention.exception.ClientException;
+import com.lee.shortlink.admin.common.enums.UserErrorCodeEnum;
+import com.lee.shortlink.admin.dao.entity.UserDO;
+import com.lee.shortlink.admin.dao.mapper.UserMapper;
+import com.lee.shortlink.admin.dto.resp.UserRespDTO;
+import com.lee.shortlink.admin.service.UserService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+/**
+ * 用户接口实现层
+ */
+@Service
+public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements UserService {
+
+    @Override
+    public UserRespDTO getUserByUsername(String username) {
+        LambdaQueryWrapper<UserDO> queryWrapper = Wrappers.lambdaQuery(UserDO.class)
+                .eq(UserDO::getUsername, username);
+        UserDO userDO = baseMapper.selectOne(queryWrapper);
+        if(userDO == null){
+            throw new ClientException(UserErrorCodeEnum.USER_NULL);
+        }
+        UserRespDTO result = new UserRespDTO();
+        BeanUtils.copyProperties(userDO, result);
+        return result;
+    }
+}
