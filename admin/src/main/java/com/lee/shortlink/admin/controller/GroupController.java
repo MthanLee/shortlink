298ee54783853f615cdf3c986fa6bbb9ace9e1aa
Lee <2014140285@qq.com>
2023-11-21 12:44:44 +0800

    feature: 开发短链接后管系统分组集合

--- admin/src/main/java/com/lee/shortlink/admin/controller/GroupController.java
+++ admin/src/main/java/com/lee/shortlink/admin/controller/GroupController.java
@@ -3,11 +3,15 @@ package com.lee.shortlink.admin.controller;
 import com.lee.shortlink.admin.common.convention.result.Result;
 import com.lee.shortlink.admin.common.convention.result.Results;
 import com.lee.shortlink.admin.dto.req.ShortLinkGroupReqDTO;
+import com.lee.shortlink.admin.dto.resp.ShortLinkGroupRespDTO;
 import com.lee.shortlink.admin.service.GroupService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * 短链接分组控制层
@@ -18,9 +22,21 @@ public class GroupController {
 
     private final GroupService groupService;
 
+    /**
+     * 新增短链接分组
+     */
     @PostMapping("/api/short-link/admin/v1/group")
     public Result<Void> save(@RequestBody ShortLinkGroupReqDTO requestParam) {
         groupService.saveGroup(requestParam.getName());
         return Results.success();
     }
+
+    /**
+     * 新增短链接分组
+     */
+    @GetMapping("/api/short-link/admin/v1/group")
+    public Result<List<ShortLinkGroupRespDTO>> listGroup() {
+        return Results.success(groupService.listGroup());
+    }
+
 }
--- admin/src/main/java/com/lee/shortlink/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/com/lee/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -1,14 +1,18 @@
 package com.lee.shortlink.admin.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.lee.shortlink.admin.dao.entity.GroupDO;
 import com.lee.shortlink.admin.dao.mapper.GroupMapper;
+import com.lee.shortlink.admin.dto.resp.ShortLinkGroupRespDTO;
 import com.lee.shortlink.admin.service.GroupService;
 import com.lee.shortlink.admin.toolkit.RandomGenerator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * 短链接分组接口实现层
@@ -29,6 +33,17 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
                 .name(groupName)
                 .build();
         baseMapper.insert(groupDO);
+    }
+
+    @Override
+    public List<ShortLinkGroupRespDTO> listGroup() {
+        // TODO 获取用户名
+        LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
+                .eq(GroupDO::getDelFlag, 0)
+                .eq(GroupDO::getUsername, "lee")
+                .orderByDesc(GroupDO::getSortOrder, GroupDO::getUpdateTime);
+        List<GroupDO> groupDOList = baseMapper.selectList(queryWrapper);
+        return BeanUtil.copyToList(groupDOList, ShortLinkGroupRespDTO.class);
     }
 
     private boolean hasGid(String gid) {
