a402ca0c8def1186aaf05c33c02a244bac8f8cc1
Lee <2014140285@qq.com>
2023-11-21 15:02:23 +0800

    feature: 修改创建请求实体名称

--- admin/src/main/java/com/lee/shortlink/admin/controller/GroupController.java
+++ admin/src/main/java/com/lee/shortlink/admin/controller/GroupController.java
@@ -2,14 +2,12 @@ package com.lee.shortlink.admin.controller;
 
 import com.lee.shortlink.admin.common.convention.result.Result;
 import com.lee.shortlink.admin.common.convention.result.Results;
-import com.lee.shortlink.admin.dto.req.ShortLinkGroupReqDTO;
+import com.lee.shortlink.admin.dto.req.ShortLinkGroupSaveReqDTO;
+import com.lee.shortlink.admin.dto.req.ShortLinkGroupUpdateReqDTO;
 import com.lee.shortlink.admin.dto.resp.ShortLinkGroupRespDTO;
 import com.lee.shortlink.admin.service.GroupService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,7 +24,7 @@ public class GroupController {
      * 新增短链接分组
      */
     @PostMapping("/api/short-link/admin/v1/group")
-    public Result<Void> save(@RequestBody ShortLinkGroupReqDTO requestParam) {
+    public Result<Void> save(@RequestBody ShortLinkGroupSaveReqDTO requestParam) {
         groupService.saveGroup(requestParam.getName());
         return Results.success();
     }
@@ -37,6 +35,15 @@ public class GroupController {
     @GetMapping("/api/short-link/admin/v1/group")
     public Result<List<ShortLinkGroupRespDTO>> listGroup() {
         return Results.success(groupService.listGroup());
+    }
+
+    /**
+     * 修改短链接分组
+     */
+    @PutMapping("/api/short-link/admin/v1/group")
+    public Result<Void> updateGroup(@RequestBody ShortLinkGroupUpdateReqDTO requestParam) {
+        groupService.updateGroup(requestParam);
+        return Results.success();
     }
 
 }
