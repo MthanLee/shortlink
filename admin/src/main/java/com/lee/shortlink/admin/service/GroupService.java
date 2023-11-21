d75fe05c107caf923e40ad6b24fe73fef965c091
Lee <2014140285@qq.com>
2023-11-21 15:03:30 +0800

    feature: 开发短链接后管系统短链接分组修改功能

--- admin/src/main/java/com/lee/shortlink/admin/service/GroupService.java
+++ admin/src/main/java/com/lee/shortlink/admin/service/GroupService.java
@@ -2,6 +2,7 @@ package com.lee.shortlink.admin.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.lee.shortlink.admin.dao.entity.GroupDO;
+import com.lee.shortlink.admin.dto.req.ShortLinkGroupUpdateReqDTO;
 import com.lee.shortlink.admin.dto.resp.ShortLinkGroupRespDTO;
 
 import java.util.List;
@@ -24,4 +25,11 @@ public interface GroupService extends IService<GroupDO> {
      * @return 用户短链接分组集合
      */
     List<ShortLinkGroupRespDTO> listGroup();
+
+    /**
+     * 修改短链接分组名
+     *
+     * @param requestParam 修改短链接分组参数
+     */
+    void updateGroup(ShortLinkGroupUpdateReqDTO requestParam);
 }
