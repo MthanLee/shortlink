7d766a6b75666e11e05de5ac212de84d44fffa82
Lee <2014140285@qq.com>
2023-11-21 12:13:45 +0800

    feature: 开发短链接后管分组新增功能

--- admin/src/main/java/com/lee/shortlink/admin/controller/GroupController.java
+++ admin/src/main/java/com/lee/shortlink/admin/controller/GroupController.java
@@ -1,7 +1,12 @@
 package com.lee.shortlink.admin.controller;
 
+import com.lee.shortlink.admin.common.convention.result.Result;
+import com.lee.shortlink.admin.common.convention.result.Results;
+import com.lee.shortlink.admin.dto.req.ShortLinkGroupReqDTO;
 import com.lee.shortlink.admin.service.GroupService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,4 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class GroupController {
 
     private final GroupService groupService;
+
+    @PostMapping("/api/short-link/admin/v1/group")
+    public Result<Void> save(@RequestBody ShortLinkGroupReqDTO requestParam) {
+        groupService.saveGroup(requestParam.getName());
+        return Results.success();
+    }
 }
--- admin/src/main/java/com/lee/shortlink/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/com/lee/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -1,9 +1,12 @@
 package com.lee.shortlink.admin.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.lee.shortlink.admin.dao.entity.GroupDO;
 import com.lee.shortlink.admin.dao.mapper.GroupMapper;
 import com.lee.shortlink.admin.service.GroupService;
+import com.lee.shortlink.admin.toolkit.RandomGenerator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -13,4 +16,26 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implements GroupService {
+
+    @Override
+    public void saveGroup(String groupName) {
+        String gid;
+        do {
+            gid = RandomGenerator.generateRandom();
+        } while (!hasGid(gid));
+        GroupDO groupDO = GroupDO.builder()
+                .gid(gid)
+                .name(groupName)
+                .build();
+        baseMapper.insert(groupDO);
+    }
+
+    private boolean hasGid(String gid) {
+        LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
+                .eq(GroupDO::getId, gid)
+                // TODO 设置用户名
+                .eq(GroupDO::getUsername, null);
+        GroupDO hasGroupFlag = baseMapper.selectOne(queryWrapper);
+        return hasGroupFlag == null;
+    }
 }
