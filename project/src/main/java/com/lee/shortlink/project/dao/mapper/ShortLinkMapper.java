533f2c0071a18207d28ba1cc4f439ba57cab36c2
Lee <2014140285@qq.com>
2023-11-22 17:33:02 +0800

    feature: 开发短链接项目创建短链接功能

---
+++ project/src/main/java/com/lee/shortlink/project/dao/mapper/ShortLinkMapper.java
@@ -0,0 +1,10 @@
+package com.lee.shortlink.project.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lee.shortlink.project.dao.entity.ShortLinkDO;
+
+/**
+ * 短链接持久层
+ */
+public interface ShortLinkMapper extends BaseMapper<ShortLinkDO> {
+}
