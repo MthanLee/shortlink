ed15ba2f2b296c38828da2373477ffdc4017ae8c
Lee <2014140285@qq.com>
2023-11-23 17:54:42 +0800

    feature: 短链接后管系统调用中台请求相关接口

---
+++ admin/src/main/java/com/lee/shortlink/admin/controller/ShortLinkController.java
@@ -0,0 +1,39 @@
+package com.lee.shortlink.admin.controller;
+
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.lee.shortlink.admin.common.convention.result.Result;
+import com.lee.shortlink.admin.remote.dto.ShortLinkRemoteService;
+import com.lee.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
+import com.lee.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
+import com.lee.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
+import com.lee.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 短链接控制层
+ */
+@RestController
+public class ShortLinkController {
+
+    ShortLinkRemoteService shortLinkRemoteService = new ShortLinkRemoteService() {};
+
+    /**
+     * 创建短链接
+     */
+    @PostMapping("/api/short-link/admin/v1/create")
+    public Result<ShortLinkCreateRespDTO> createShortLink(@RequestBody ShortLinkCreateReqDTO requestParam) {
+        return shortLinkRemoteService.createShortLink(requestParam);
+    }
+
+    /**
+     * 分页查询短链接
+     */
+    @GetMapping("/api/short-link/admin/v1/page")
+    public Result<IPage<ShortLinkPageRespDTO>> pageShortLink(ShortLinkPageReqDTO requestParam) {
+        return shortLinkRemoteService.pageShortLink(requestParam);
+    }
+}
