213e91f27688811ceff984be9937d4c6b3fff395
Lee <2014140285@qq.com>
2023-11-24 18:11:37 +0800

    feature: 短链接系统修改短链接功能

--- project/src/main/java/com/lee/shortlink/project/controller/ShortLinkController.java
+++ project/src/main/java/com/lee/shortlink/project/controller/ShortLinkController.java
@@ -5,6 +5,7 @@ import com.lee.shortlink.project.common.convention.result.Result;
 import com.lee.shortlink.project.common.convention.result.Results;
 import com.lee.shortlink.project.dto.req.ShortLinkCreateReqDTO;
 import com.lee.shortlink.project.dto.req.ShortLinkPageReqDTO;
+import com.lee.shortlink.project.dto.req.ShortLinkUpdateReqDTO;
 import com.lee.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
 import com.lee.shortlink.project.dto.resp.ShortLinkGroupCountQueryRespDTO;
 import com.lee.shortlink.project.dto.resp.ShortLinkPageRespDTO;
@@ -29,6 +30,15 @@ public class ShortLinkController {
     @PostMapping("/api/short-link/v1/create")
     public Result<ShortLinkCreateRespDTO> createShortLink(@RequestBody ShortLinkCreateReqDTO requestParam) {
         return Results.success(shortLinkService.createShortLink(requestParam));
+    }
+
+    /**
+     * 修改短链接
+     */
+    @PostMapping("/api/short-link/admin/v1/update")
+    public Result<Void> updateShortLink(@RequestBody ShortLinkUpdateReqDTO requestParam) {
+        shortLinkService.updateShortLink(requestParam);
+        return Results.success();
     }
 
     /**
