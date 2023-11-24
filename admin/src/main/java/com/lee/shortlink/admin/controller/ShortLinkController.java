1ab83f4c4175aeea7c67fc32bdb9af6ff142e4bb
Lee <2014140285@qq.com>
2023-11-24 18:53:04 +0800

    feature: 短链接后管系统修改短链接功能

--- admin/src/main/java/com/lee/shortlink/admin/controller/ShortLinkController.java
+++ admin/src/main/java/com/lee/shortlink/admin/controller/ShortLinkController.java
@@ -3,9 +3,11 @@ package com.lee.shortlink.admin.controller;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.lee.shortlink.admin.common.convention.result.Result;
+import com.lee.shortlink.admin.common.convention.result.Results;
 import com.lee.shortlink.admin.remote.dto.ShortLinkRemoteService;
 import com.lee.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
 import com.lee.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
+import com.lee.shortlink.admin.remote.dto.req.ShortLinkUpdateReqDTO;
 import com.lee.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
 import com.lee.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,6 +33,15 @@ public class ShortLinkController {
     @PostMapping("/api/short-link/admin/v1/create")
     public Result<ShortLinkCreateRespDTO> createShortLink(@RequestBody ShortLinkCreateReqDTO requestParam) {
         return shortLinkRemoteService.createShortLink(requestParam);
+    }
+
+    /**
+     * 修改短链接
+     */
+    @PostMapping("/api/short-link/admin/v1/update")
+    public Result<Void> updateShortLink(@RequestBody ShortLinkUpdateReqDTO requestParam) {
+        shortLinkRemoteService.updateShortLink(requestParam);
+        return Results.success();
     }
 
     /**
--- project/src/main/java/com/lee/shortlink/project/controller/ShortLinkController.java
+++ project/src/main/java/com/lee/shortlink/project/controller/ShortLinkController.java
@@ -35,7 +35,7 @@ public class ShortLinkController {
     /**
      * 修改短链接
      */
-    @PostMapping("/api/short-link/admin/v1/update")
+    @PostMapping("/api/short-link/v1/update")
     public Result<Void> updateShortLink(@RequestBody ShortLinkUpdateReqDTO requestParam) {
         shortLinkService.updateShortLink(requestParam);
         return Results.success();
